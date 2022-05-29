4296554415b0a58cc7404f481f24fdf7bdd9ee4b
Ilya-Fokin <fokin3349@mail.ru>
2022-05-29 10:55:58 +0400

    Front commit

---
+++ src/main/resources/static/js/js.js
@@ -0,0 +1,10 @@
+
+$(".menu_bar").click(function() {
+    $(".menu_zone").css("transition", ".3s");
+    $(".menu_zone").css("left", "0");
+})
+
+$(".cl-btn-3").click(function() {
+    $(".menu_zone").css("transition", ".3s");
+    $(".menu_zone").css("left", "-250px");
+})
